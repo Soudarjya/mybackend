ce00d84fee5ff87561d601e89f558883df6d323b
Soudarjya <soudyg707@gmail.com>
2024-10-23 23:24:55 +0530

    commit

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect('mongodb://localhost:27017/DB');
         console.log('MongoDB Connected');
     } catch (error) {
         console.error('MongoDB connection error:', error);
--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-const express = require('express')
+// server.js 
+
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const productRoutes = require('./routes/productRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
+// dotenv.config();
+const cors=require('cors');
+connectDB();
+
 const app = express();
-const dotenv=require('dotenv');
-dotenv.config();
-const mongoose=require('mongoose');
-
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.get('/',(req,res)=>{
+    res.send('RAand')
 })
+app.use(cors());
+app.use(express.json());
+app.use('/api/users',authRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/orders', orderRoutes);
 
-app.listen(3000);
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT =5000;
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
