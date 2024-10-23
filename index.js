4d004e1272eacc65e8b95dc50e67749f9fe0d0ee
Soudarjya <soudyg707@gmail.com>
2024-10-23 16:39:58 +0530

    initial commmit

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
-
+const mongoose=require('mongoose');
+mongoose.connect('mongodb://localhost:27017/DB',()=>{
+   console.log('database connected');
+    
+})
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
