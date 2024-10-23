457c51f1e52c750c6092aca1727116931299e160
Soudarjya <soudyg707@gmail.com>
2024-10-23 18:00:21 +0530

    initial commmit

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require('express')
 const app = express()
 const mongoose=require('mongoose');
-mongoose.connect('mongodb://jlt-phi.vercel.app:27017/DB',()=>{
-   console.log('database connected');
-    
-})
+mongoose.connect('mongodb://localhost:27017/DB')
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
