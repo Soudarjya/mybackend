c540dbdff5442fa61bee84c26cfcb907fa353c60
Soudarjya <soudyg707@gmail.com>
2024-10-23 22:51:29 +0530

    commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
-const app = express()
+const app = express();
+const dotenv=require('dotenv');
+dotenv.config();
 const mongoose=require('mongoose');
-mongoose.connect('mongodb://localhost:27017/DB')
+mongoose.connect(process.env.MONGO_URI)
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
