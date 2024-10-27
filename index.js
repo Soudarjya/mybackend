15784c705afdb5a7512d1ce74986d6987ac33ab4
Soudarjya <soudyg707@gmail.com>
2024-10-27 15:27:57 +0530

    commit

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use('/api/orders', orderRoutes);
 // app.use(notFound);
 // app.use(errorHandler);
 
-const PORT =5000;
+const PORT =process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
 
