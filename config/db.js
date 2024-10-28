a19c2a1183c7ee3fdd7def3669666e5a39929e8d
Soudarjya <soudyg707@gmail.com>
2024-10-28 13:03:55 +0530

    commmit

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://soudyg707:iamajaywife@db.yoozt.mongodb.net/?retryWrites=true&w=majority&appName=DB");
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log('MongoDB Connected');
     } catch (error) {
         console.error('MongoDB connection error:', error);
